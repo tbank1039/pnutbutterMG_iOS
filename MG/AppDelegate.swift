90b57c6dc8642426153eab7335d8d7dac8b80058
Tyler Bank <tbank@mines.edu>
2017-07-06 21:11:25 -0600

    adding IQKeyboardManager

--- MG/AppDelegate.swift
+++ MG/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.sharedManager().enable = true
+        
         let controllerId = "Login"
         let storyboard: UIStoryboard = UIStoryboard(name:"Main", bundle: nil)
         let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: controllerId) as UIViewController
--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ use_frameworks!
 
 target 'MG' do
     pod 'Alamofire', '~> 4.4'
+    pod 'IQKeyboardManagerSwift', '4.0.8'
 end
